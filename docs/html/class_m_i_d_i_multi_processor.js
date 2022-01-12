d5f75996ff6516b95d57e9fc47dbb6164fb85763
Nicola Cassetta <ncassetta@tiscali.it>
2022-01-12 18:46:22 +0100

    Further tuning before ver 1.1.0
    Some little enhancements in various files:
    - driver.h Some #define made static const member of MIDIOutDriver class
    - timer.h & .cpp MIDITimer class made totally static: the constructor is deleted and no more created a dummy instance of the class
    - manager.h &.cpp changes related to MIDITimer class
    - track.h & .cpp changes in documentation, corrected a bug in method MIDITrack::GetChannel()
    - matrix.h & .cpp reorganized the note_on_count array in one dimension, so faster operations on it
    - advancedsequencer.cpp & sequences.cpp  & thru.cpp substituted all proc_loc.lock() and unlock() calls with lock_guard()
    
    Recompiled the documentation
    -

--- docs/html/class_m_i_d_i_multi_processor.js
+++ docs/html/class_m_i_d_i_multi_processor.js
@@ -2,8 +2,8 @@ var class_m_i_d_i_multi_processor =
 [
     [ "MIDIMultiProcessor", "class_m_i_d_i_multi_processor.html#a69f17c31b5cb75fb506f0f06a3057e4f", null ],
     [ "Reset", "class_m_i_d_i_multi_processor.html#a6a95120694ccbc5401b325f25c5258f3", null ],
-    [ "GetProcessor", "class_m_i_d_i_multi_processor.html#a58405bed0496b961a386a9b0a4dd350d", null ],
-    [ "GetProcessor", "class_m_i_d_i_multi_processor.html#a94afde72ecd65001d702feb14dd622ac", null ],
+    [ "GetProcessor", "class_m_i_d_i_multi_processor.html#a88147559084fac987ac4a6a60841a3e6", null ],
+    [ "GetProcessor", "class_m_i_d_i_multi_processor.html#aa8b997bf9a30bccf0ddd665bc412f52a", null ],
     [ "GetProcessMode", "class_m_i_d_i_multi_processor.html#aee7b5c06a27d947fa1de34e5eb9f15f6", null ],
     [ "SetProcessor", "class_m_i_d_i_multi_processor.html#a9a03bc0dadea40b286f8b7c7e326452d", null ],
     [ "RemoveProcessor", "class_m_i_d_i_multi_processor.html#af74262263ae37dab8f547d2eca3ae80f", null ],
--- docs/html/struct_m_i_d_i_file_header.js
+++ docs/html/struct_m_i_d_i_file_header.js
@@ -1,6 +1,5 @@
 var struct_m_i_d_i_file_header =
 [
-    [ "MIDIFileHeader", "struct_m_i_d_i_file_header.html#ac500f77512fa3915acd3b9d74c16e419", null ],
     [ "format", "struct_m_i_d_i_file_header.html#a3c4c288866ea4f09140c0d2170c3c3bb", null ],
     [ "ntrks", "struct_m_i_d_i_file_header.html#a78aad57cf05bb352f0ccbaa1a9c68596", null ],
     [ "division", "struct_m_i_d_i_file_header.html#aad2e8c41e205ebc165c4eb0cfd6f47df", null ],
